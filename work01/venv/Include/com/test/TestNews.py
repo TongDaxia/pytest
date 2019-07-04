400468824ab0f5080363fb3d98792eea5e6a420c
TongDaxia <tong_y_g@163.com>
2019-07-04 22:05:09 +0800

    retrying

--- work01/venv/Include/com/test/TestNews.py
+++ work01/venv/Include/com/test/TestNews.py
@@ -4,7 +4,7 @@ from lxml import etree
 import time
 import os
 
-os.chdir('C:/Users/Administrator/Desktop')  # 更改工作目录为桌面
+os.chdir('C:/Users/tyg/Desktop')  # 更改工作目录为桌面
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
